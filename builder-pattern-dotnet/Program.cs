c2360521ef7fb4d9d75a447c2065a67c743e8b2f
kbr <kbr@REOKOBAYASHI>
2024-01-11 00:54:51 +0900

    add program.cs

--- builder-pattern-dotnet/Program.cs
+++ builder-pattern-dotnet/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ビルダーパターンの練習
+using builder_pattern_dotnet;
+
+var b = UserBuilder.Instance
+    .SetName("小林怜雄")
+    .SetBirth(new DateTime(1998, 10, 9))
+    .SetNickname("コバレオ")
+    .Build();
+
+Console.WriteLine(b.Name);
+Console.WriteLine(b.Birth);
+Console.WriteLine(b.Nickname);
